be4a36759d5d1f23380426d59bd66214729c2ad9
Hansel <hansel@163.com>
2022-02-11 23:28:33 +0800

    modified for lvgl v8.20. Okay

--- main/main.cpp
+++ main/main.cpp
@@ -1,5 +1,4 @@
 #include <driver/gpio.h>
-#include <esp_freertos_hooks.h>
 #include <esp_system.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/semphr.h>
@@ -58,24 +57,28 @@ static void task_GUI(void* pvParameter) {
 
   lv_color_t* buf1 = (lv_color_t*)heap_caps_malloc(DISP_BUF_SIZE * sizeof(lv_color_t), MALLOC_CAP_DMA);
   assert(buf1 != NULL);
-  lv_color_t* buf2 = (lv_color_t*)heap_caps_malloc(DISP_BUF_SIZE * sizeof(lv_color_t), MALLOC_CAP_DMA);
-  assert(buf2 != NULL);
+  lv_color_t* buf2 = NULL;
 
-  static lv_disp_buf_t disp_buf;
+  static lv_disp_draw_buf_t disp_buf;
   uint32_t size_in_px = DISP_BUF_SIZE;
 
-  lv_disp_buf_init(&disp_buf, buf1, buf2, size_in_px);
+  lv_disp_draw_buf_init(&disp_buf, buf1, buf2, size_in_px);
 
   lv_disp_drv_t disp_drv;
   lv_disp_drv_init(&disp_drv);
+  disp_drv.hor_res = LV_HOR_RES_MAX;
+  disp_drv.ver_res = LV_VER_RES_MAX;
   disp_drv.flush_cb = disp_driver_flush;
+  disp_drv.draw_buf = &disp_buf;
 
-  disp_drv.buffer = &disp_buf;
   lv_disp_drv_register(&disp_drv);
 
   const esp_timer_create_args_t periodic_timer_args = {
   .callback = &task_lv_tick,
+  .arg = (void*)1,
+  .dispatch_method = ESP_TIMER_TASK,
   .name = "periodic_gui",
+  .skip_unhandled_events = false,
   };
 
   esp_timer_handle_t periodic_timer;
@@ -103,12 +106,12 @@ static void task_GUI(void* pvParameter) {
 }
 
 static void setupGUI() {
-  hello_world_label = lv_label_create(lv_scr_act(), NULL);
+  hello_world_label = lv_label_create(lv_scr_act());
   lv_label_set_text(hello_world_label, "Hello world!");
-  lv_obj_align(hello_world_label, NULL, LV_ALIGN_CENTER, 0, 0);
+  lv_obj_align(hello_world_label, LV_ALIGN_CENTER, 0, 0);
 
-  count_label = lv_label_create(lv_scr_act(), NULL);
-  lv_obj_align(count_label, NULL, LV_ALIGN_IN_BOTTOM_MID, 0, 0);
+  count_label = lv_label_create(lv_scr_act());
+  lv_obj_align(count_label, LV_ALIGN_BOTTOM_MID, 0, 0);
 }
 
 static void task_lv_tick(void* arg) {
